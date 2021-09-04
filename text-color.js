269aeae3da9c7d162d97c36628d3b89006109318
minjun lee <84955607+devkr-potato@users.noreply.github.com>
2021-09-04 21:04:37 +0900

    적힌 텍스트의 글씨 색을 바꿔서 나타냄

--- text-color.js
+++ text-color.js
@@ -1,28 +1,22 @@
 var text = document.getElementById("content");
 var str = text.innerHTML,
     reg = /red|blue|green|orange/ig; //g is to replace all occurances
-
-//fixing a bit
 var toStr = String(reg);
 var color = (toStr.replace('\/g', '|')).substring(1);
 
-//split it baby
+//쪼개지는 부분 색깔
 var colors = color.split("|");
 
 if (colors.indexOf("red") > -1) {
-    str = str.replace(/sweet/g, '<span style="color:red;">sweet</span>');
+    str = str.replace(/t/g, '<span style="color:red;">t</span>');
 }
 
 if (colors.indexOf("blue") > -1) {
-    str = str.replace(/home/g, '<span style="color:blue;">home</span>');
+    str = str.replace(/e/g, '<span style="color:blue;">e</span>');
 }
 
 if (colors.indexOf("green") > -1) {
-    str = str.replace(/heart/g, '<span style="color:green;">heart</span>');
-}
-
-if (colors.indexOf("orange") > -1) {
-    str = str.replace(/the/g, '<span style="color:orange;">the</span>');
+    str = str.replace(/x/g, '<span style="color:green;">x</span>');
 }
 
 
