7b613b5a9764206404542c2eefa2a83183603cb6
Minjun <84955607+devkr-potato@users.noreply.github.com>
2022-04-24 20:29:15 +0900

    Update and rename localStorage.html to script/localStorage.js

---
+++ script/localStorage.js
@@ -0,0 +1,8 @@
+// 로컬스토리지에 저장하기 | getItem(Key, Value)
+window.localStorage.setItem('name','JS');
+
+// 로컬스토리지에 있는 내용 Key 로 가져오기
+window.localStorage.getItem('name');
+
+// 로컬스토리지에 저장된 Key와 Value 삭제하기
+window.localStorage.removeItem('name');
